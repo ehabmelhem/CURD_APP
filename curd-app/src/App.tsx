6d4751596c9bb87adcc2703768ffde5e3b1bdc63
ehab <Melhemehab@gmail.com>
2021-03-04 19:14:30 +0200

    set up React

---
+++ curd-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <>
+   {/* ADD */}
+   {/* Get */}
+   {/* update */}
+   {/* Delete */}
+    </>
+  )
+}
+
+export default App;
---
+++ curd-app/src/Compents/Add.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Add: React.FC = () => {
+    return (
+        <h1>this is Add compent</h1>
+    )
+};
+
+export default Add;
